2c5e147ff8692f37f58faa0934addd70ee2751f7
Greek4Sea <pagkratis@gmail.com>
2026-07-16 22:47:06 -0400

    split training and validation set

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ from sklearn.tree import DecisionTreeRegressor
 #and sklearn for the DecisionTreeRegressor which is the model we will train on
 
 TARGET = "Seen_Allergist"
-
+STATE = 42 #seed to lock the random number generator
 #to load the data
 
 root = Path(__file__).resolve().parents[1]
@@ -24,12 +24,22 @@ combined = pd.concat([features, test_features], keys=["train", "test"])
 for i in combined.columns:
     if not pd.api.types.is_numeric_dtype(combined[i]):
         nums, _ = pd.factorize(combined[col])
-        nums = nums.astype(float)
+        nums = nums.astype(float) #convert all to floats
         for i in range(len(nums)): #if there is a missing vaue then it replaces it with NaN, since the factorizing will turn it into -1
             if nums[i] == -1:
                 nums[i] = np.nan
 
+combined = combined.fillna(-1000)#this fills in NaN with a number so stupid that the model learns to not account for it
+X = combined.loc["train"].to_numpy() #these two seperat the data we had factorized into its training and testing sets since they had gotten links
+X_test = combined.loc["test"].to_numpy()
 
+# we will split the trianing data into a randomized 80 - 20 split
+random_data = np.random.RandomState(STATE)
+shuffle_patients_index = random_data.permutation(len(X))
+N_validation = len(X) // 5 #splitting into 20% for validation
+#break up the index numbers into validation and training
+validation_index = shuffled[:N_validation]
+training_index = shuffled[N_validation:]
 
 
 
